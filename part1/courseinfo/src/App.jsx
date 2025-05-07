caa9d3d402c8e12659f92ccb4540ff1d35d741ed
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-05-07 20:03:40 +0100

    exercise1.2-redo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,14 +4,20 @@ const Header = ({ course }) => {
   )
 }
 
+const Part = ({ part, exercises }) => {
+  return (
+    <div>
+      {part} {exercises}
+    </div>
+  )
+}
+
 const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
   return (
     <div>
-      {part1} {exercises1}
-      <br></br>
-      {part2} {exercises2}
-      <br></br>
-      {part2} {exercises2}
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
     </div>
   )
 }
