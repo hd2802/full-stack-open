0f2552c16ef3e077b0279e550ea126d1b8b7e007
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-05-07 10:04:25 +0100

    exercise-1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,69 +1,48 @@
 import { useState } from 'react'
 
-const Rundown = ({good, neutral, bad}) => {
-  return (
-    <div>
-        good {good} <br></br>
-        neutral {neutral} <br></br>
-        bad {bad}
-    </div>
+const Button = ({ handleClick, text}) => {
+  return(
+    <button onClick={handleClick}>
+      {text}
+    </button>
   )
 }
 
-const Total = ({ good, neutral, bad }) => {
+const StatisticLine = ({ text, value }) => {
   return (
-    <div>
-      all {good + neutral + bad}
-    </div>
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
   )
-}
-
-const PercentagePositive = ({ good, neutral, bad }) => {
-  if (neutral + bad !== 0) {
-    return (
-      <div>
-        positive {( good / (neutral + bad )) * 100} %
-      </div>
-    )
-  }
-  else {
-    return (
-      <div>
-        positive 0 %
-      </div>
-    )
-  }
-}
-
-// error occurred here when tried to reassign const total by using += 
-const Average = ({ good, neutral, bad }) => {
-  const total = good * 1 + bad * -1;
-  const totalFeedback = good + neutral + bad;
-  const average = totalFeedback !== 0 ? total / totalFeedback : 0;
-
-  return (
-    <div>
-      average {average}
-    </div>
-  );
 }
 
 const Statistics = (props) => {
-  if (props.good !== 0 || props.neutral !== 0 || props.bad !== 0) {
-    return (
+  if (props.good === 0 && props.neutral === 0 && props.bad === 0){
+    return ( 
       <div>
-          <Rundown good={props.good} neutral={props.neutral} bad={props.bad} />
-          <Total good={props.good} neutral={props.neutral} bad={props.bad} />
-          <PercentagePositive good={props.good} neutral={props.neutral} bad={props.bad} />
-          <Average good={props.good} neutral={props.neutral} bad={props.bad} />
+        No feedback given
       </div>
     )
   }
   else {
     return (
-      <div>
-        No feedback given
-      </div>
+      <table>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        <StatisticLine text="all" value={props.good + props.neutral + props.bad} />
+        <StatisticLine text="average" value={
+            props.good + props.neutral + props.bad === 0
+              ? 0
+              : ((props.good * 1 + props.bad * -1) / (props.good + props.bad + props.neutral))
+          }
+         />
+        <StatisticLine text="positive" value={
+          props.neutral + props.bad === 0 ? 0 :
+          ((props.good) / (props.neutral + props.bad)) * 100
+        } />
+     </table>
     )
   }
 }
@@ -73,21 +52,26 @@ const App = () => {
   const [ neutral, setNeutral ] = useState(0)
   const [ bad, setBad ] = useState(0)
 
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
       <div>
-        <button onClick={() => setGood(good + 1)}>
-          good
-        </button>
-        <button onClick={() => setNeutral(neutral + 1)}>
-          neutral
-        </button>
-        <button onClick={() => setBad(bad + 1)}>
-          bad
-        </button>
+        <Button text={"good"} handleClick={handleGood} />
+        <Button text={"neutral"} handleClick={handleNeutral} />
+        <Button text={"bad"} handleClick={handleBad} />
       </div>
-
       <h1> statistics </h1>
       <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
