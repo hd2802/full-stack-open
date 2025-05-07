76761f40df31632fa1fbb265d1f22fd7d0009bfe
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-05-07 20:32:35 +0100

    redo exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,21 +1,75 @@
 import { useState } from 'react'
 
+const Average = ({ good, bad, total }) => {
+  if (total === 0) {
+    return (
+      <div>
+        average 0
+      </div>
+    )
+  }
+  else {
+    return (
+      <div>
+        average {(good + bad * -1 )/ total}
+      </div>
+    )
+  }
+}
+
+const Percentage = ({ good, total }) => {
+  // why is this a let and not a const?
+  let percentage = 0
+  if (total === 0) {
+    percentage = 0
+  }
+  else {
+    percentage = (good / total) * 100
+  }
+
+  return (
+    <div>
+      percentage {percentage}
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const handleClick = () => {
+    setTotal(total + 1)
+  }
+
+  const handleGood = () => {
+    setGood(good + 1)
+    handleClick()
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+    handleClick()
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+    handleClick()
+  }
 
   return (
     <div>
       <h1>give feedback</h1>
       <div>
-        <button onClick={() => setGood(good + 1)}>
+        <button onClick={handleGood}>
           good
         </button>
-        <button onClick={() => setNeutral(neutral + 1)}>
+        <button onClick={handleNeutral}>
           neutral
         </button>
-        <button onClick={() => setBad(bad + 1)}>
+        <button onClick={handleBad}>
           bad
         </button>
       </div>
@@ -26,6 +80,10 @@ const App = () => {
     neutral {neutral}
     <br></br>
     bad {bad}
+    <br></br>
+    all {total}
+    <Average good={good} bad={bad} total={total} />
+    <Percentage good={good} total={total} />
     </div>
   )
 }
