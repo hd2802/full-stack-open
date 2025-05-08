ffa73dc3d67109159f9dc3c4e53a79eb41100806
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-05-08 13:52:26 +0100

    exercise 2.9: add search function to phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -8,25 +8,37 @@ const Details = ({ name, number }) => {
   )
 }
 
+const PersonList = ({ listofpeople }) => {
+  return (
+    <ul>
+    {listofpeople.map(entry =>
+      <Details key={entry.name} name={entry.name} number={entry.number} />
+    )}
+  </ul>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '1010100110010'
-    }
-  ]) 
+    // dummy data for search feature testing
+      { name: 'Arto Hellas', number: '040-123456', id: 1 },
+      { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+      { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+      { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    ])
+  const [filtered, setFiltered] = useState(persons)
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
   const addNewPerson = (event) => {
     event.preventDefault()
-    console.log(persons)
+    //console.log(persons)
     if (persons.some(e => e.name === newName)) {
       alert(`${newName} is already added to phonebook`)
     }
   
     else {
-      if (newNumber !== '' && newName !== '')
-      {
+      if (newNumber !== '' && newName !== '') {
         const personObject = {
           name: newName,
           number: newNumber,
@@ -37,22 +49,30 @@ const App = () => {
         setNewName('')
         setNewNumber('')
       }
-      
     }
   }
 
+  const handleSearch = (event) => {
+    event.preventDefault()
+    setFiltered(persons.filter((e) => e.name.toLowerCase().includes(event.target.value.toLowerCase())))
+  }
+
   const handleInputChange = (event) => {
+    event.preventDefault()
     setNewName(event.target.value)
   }
 
   const handleInputChangeNumber = (event) => {
+    event.preventDefault()
     setNewNumber(event.target.value)
   }
 
   return (
     <div>
-      <h2>Phonebook</h2>
-
+      <h1>Phonebook</h1>
+      filter shown with 
+      <input onChange={handleSearch} />
+      <h2>add new</h2>
       <form onSubmit={addNewPerson}>
         <div>
           name: <input onChange={handleInputChange}/>
@@ -66,9 +86,7 @@ const App = () => {
       </form>
 
       <h2>Numbers</h2>
-      {persons.map(person =>
-          <Details key={person.name} name={person.name} number={person.number}/>
-      )}
+        <PersonList listofpeople={filtered} />
     </div>
   )
 }
