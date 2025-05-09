62f40714a7416967c6a0f73f5e4e4c3de29766d4
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-05-09 21:09:29 +0100

    exercise 2.12: set up post json server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -40,8 +40,15 @@ const App = () => {
           number: newNumber,
           id: String(persons.length + 1),
         }
+
+        axios
+          .post('http://localhost:3001/persons', personObject)
+          .then(response => {
+            setPersons(persons.concat(response.data))
+          })
       
         setPersons(persons.concat(personObject))
+        setFiltered(filtered.concat(personObject))
         setNewName('')
         setNewNumber('')
       }
