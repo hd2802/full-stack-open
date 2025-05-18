92a604c4fad74b3ac6770ef07a2c8dfea96e7204
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-05-18 11:44:57 +0100

    2.1&2.2: courseinfo separate Course component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,76 +1,31 @@
-// Ex 1.1: Refactor the code so we have Header, Content and Total components
-
-const Header = ({ course }) => {
-  return (
-    <h1>
-      {course.name}
-    </h1>
-  )
-}
-
-const Part = ({ part }) => {
-  //console.log(part)
-  return (
-    <div>
-      {part.name} {part.exercises}
-    </div>
-  )
-}
-
-const Content = ({ course }) => {
-  // forEach does not return anything so it isnt applicable here
-  return (
-    <div>
-      {
-        course.parts.map((part, index) => (
-          <div key={index}>
-            <Part part={part} />
-          </div>
-        ))
-      }
-    </div>
-  )
-}
-
-const Total = ({ course }) => {
-  let total = 0
-  return (
-    <div>
-      Total: 
-      {
-        course.parts.forEach((part) => {
-          total += part.exercises 
-        })
-      }
-      {total}
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
 const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course}/>
-      <Total course={course}/>
+      <Course course={course} />
     </div>
   )
 }
---
+++ part1/courseinfo/src/components/Course.jsx
@@ -0,0 +1,59 @@
+const Header = ({ course }) => {
+  return (
+    <h1>
+      {course.name}
+    </h1>
+  )
+}
+
+const Part = ({ part }) => {
+  //console.log(part)
+  return (
+    <div>
+      {part.name} {part.exercises}
+    </div>
+  )
+}
+
+const Content = ({ course }) => {
+  // forEach does not return anything so it isnt applicable here
+  return (
+    <div>
+      {
+        course.parts.map((part, index) => (
+          <div key={index}>
+            <Part part={part} />
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+const Total = ({ course }) => {
+  let total = 0
+  return (
+    <div>
+      {
+        course.parts.forEach((part) => {
+          total += part.exercises 
+        })
+      }
+      <b>
+      total of {total} exercises
+      </b>
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+    return (
+    <div>
+      <Header course={course} />
+      <Content course={course}/>
+      <Total course={course}/>
+    </div>
+    )
+}
+
+export default Course
