0d177711f6465cf0c3996dc85a7d0ac5b1ef339b
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-05-22 10:46:42 +0100

    3.4: add delete functionality to phonebook backend

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -49,6 +49,13 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = requests.params.id
+  person = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
