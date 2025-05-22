65c023be6b92bdfcc467638e9b46ee3d48bd50d8
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-05-22 10:36:48 +0100

    3.1: use Express for phonebook backend

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,4 +1,5 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
 let persons = [
     { 
@@ -23,11 +24,15 @@ let persons = [
     }
 ]
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(persons))
+app.get('/', (request, response) => {
+  response.send('<h1>Hello world</h1>')
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const PORT = 3002
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
