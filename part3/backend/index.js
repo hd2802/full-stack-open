bbd38f44faf41c914b2d9c5e3db861f61ce59877
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-05-25 17:59:59 +0100

    3.16: moves error handling to a new error handler middleware

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -25,6 +25,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 app.use(express.json())
 
+
 morgan.token('body', (req) => {
   return req.method === 'POST' ? JSON.stringify(req.body) : ''
 })
@@ -93,8 +94,24 @@ app.delete('/persons/:id', (request, response) => {
     })
 })
 
-
 const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+app.use(errorHandler)
