c8bdaade391f90c9a3b9a327acad7c2629f99b01
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-05-27 21:46:03 +0100

    3.2: adds /info page for displaying statistics of json data

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -37,6 +37,11 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    response.send(`<div>Phonebook has info for ${persons.length} people <br />
+        ${Date()}</div>`)
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
