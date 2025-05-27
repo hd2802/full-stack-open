14a566048c6f052b6d0ed8e537accf668b4a0c8f
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-05-27 14:54:52 +0100

    4.9: adds test for ensuring that the _id property is renamed correctly

--- part4/backend/tests/blog_api.test.js
+++ part4/backend/tests/blog_api.test.js
@@ -20,6 +20,18 @@ test('all notes are returned', async () => {
     assert.strictEqual(response.body.length, CURRENT_NOTE_COUNT)
 })
 
+test('returned notes should have the valid id property', async () => {
+    const response = await api.get('/api/blogs')
+    let all_valid = true
+    response.body.forEach((blog) => {
+        // need now to check that each blog has _id renamed
+        if (!Object.hasOwn(blog, 'id')) {
+            all_valid = false
+        }
+    })
+    assert.strictEqual(all_valid, true)
+})
+
 after(async () => {
     await mongoose.connection.close()
 })
