f77910f96d9a67b482916db45ff56b0c418459b4
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-05-27 10:57:44 +0100

    4.4: add totalLikes function and test for function

--- part4/backend/tests/list_helper.test.js
+++ part4/backend/tests/list_helper.test.js
@@ -1,9 +1,24 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
+
+// custom imports
 const listHelper = require('../utils/list_helper')
+const data = require('./test_data')
 
 test('dummy returns one', () => {
     const blogs = []
     const result = listHelper.dummy(blogs)
     assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+    test('when list has only one blog, equals the likes of that blog', () => {
+        const result = listHelper.totalLikes(data.singleBlogList)
+        assert.strictEqual(result, 5)
+    })
+
+    test('when list has multiple blogs, equals the total likes of the blogs', () => {
+        const result = listHelper.totalLikes(data.blogs)
+        assert.strictEqual(result, 36)
+    })
 })
