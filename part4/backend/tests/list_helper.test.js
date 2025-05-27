1ff5518d1712a267aac49f73dad4d3a7ccd925ae
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-05-27 11:06:52 +0100

    4.4: totalLikes handles empty array correctly

--- part4/backend/tests/list_helper.test.js
+++ part4/backend/tests/list_helper.test.js
@@ -7,17 +7,22 @@ const data = require('./test_data')
 
 test('dummy returns one', () => {
     const blogs = []
-    const result = listHelper.dummy(blogs)
+    const result = listHelper.dummy()
     assert.strictEqual(result, 1)
 })
 
 describe('total likes', () => {
+    test('of empty list is zero', () => {
+        const result = listHelper.totalLikes([])
+        assert.strictEqual(result, 0)
+    })
+
     test('when list has only one blog, equals the likes of that blog', () => {
         const result = listHelper.totalLikes(data.singleBlogList)
         assert.strictEqual(result, 5)
     })
 
-    test('when list has multiple blogs, equals the total likes of the blogs', () => {
+    test('of a bigger list is calculated correctly', () => {
         const result = listHelper.totalLikes(data.blogs)
         assert.strictEqual(result, 36)
     })
