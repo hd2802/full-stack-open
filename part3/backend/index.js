9567276bafc1532b0e46337be77e11f766716121
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-05-28 11:47:11 +0100

    configures alternative port

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -83,7 +83,8 @@ app.post('/api/persons', (request, response) => {
   response.json(newPerson)
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = /* process.env.PORT || */3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
 
