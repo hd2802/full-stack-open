4f80c533082fa5b712ff256271ce1abad70b56e6
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-05-29 11:57:51 +0100

    4.5: adds favouriteBlog function and tests for favouriteBlog

--- part4/backend/utils/list_helper.js
+++ part4/backend/utils/list_helper.js
@@ -2,6 +2,28 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const result = blogs.reduce((acc, blog) => {
+        return acc + blog.likes
+    }, 0 )
+    return result
+}
+
+const favouriteBlog = (blogs) => {
+    if (blogs.length === 0) {return null}
+    const favouriteBlog = blogs.reduce((prev, current) => {
+        return prev.likes > current.likes ? prev : current
+    })
+    return {
+        title: favouriteBlog.title,
+        author: favouriteBlog.author,
+        url: favouriteBlog.url,
+        likes: favouriteBlog.likes
+    }
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favouriteBlog
 }
