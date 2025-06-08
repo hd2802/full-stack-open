851ed65b5c1a353f283cbe8b209a65c098289256
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-06-08 15:22:39 +0100

    4.2: separates backend into components

---
+++ part4/backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+// middleware and config imports
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./middleware/middleware')
+
+// router imports
+const blogsRouter = require('./controllers/blogs')
+
+// app config
+const app = express()
+app.use(express.json())
+
+const mongoUrl = config.MONGODB_URI
+
+try {
+    mongoose.connect(mongoUrl)
+} catch (error) {
+    logger.info('error connecting to MongoDB: ', error)
+}
+
+// router connections
+app.use('/api/blogs', blogsRouter)
+
+// middleware connections
+app.use(middleware.requestLogger)
+
+module.exports = app
