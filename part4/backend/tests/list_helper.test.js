54e480bdcf8d087943250665e2935b70eaa4d0b2
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-06-09 15:18:03 +0100

    4.4: totalLikes added and tested correctly

--- part4/backend/tests/list_helper.test.js
+++ part4/backend/tests/list_helper.test.js
@@ -1,11 +1,39 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 
+// Importing module to test
 const listHelper = require('../utils/list_helper')
+
+// Importing test data
+const blogs_data = require('../data/test_blogs')
 
 test('dummy returns one', () => {
     const blogs = []
 
     const result = listHelper.dummy(blogs)
     assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+    
+    test('of empty list is zero', () => {
+        const blogs = []
+
+        const result = listHelper.totalLikes(blogs)
+        assert.strictEqual(result, 0)
+    })
+
+    test('when list has only one blog equals the likes of that', () => {
+        const blogs = blogs_data.singleton_blog
+
+        const result = listHelper.totalLikes(blogs)
+        assert.strictEqual(blogs[0].likes, result)
+    })
+
+    test('of a bigger list is calculated right', () => {
+        const blogs = blogs_data.blogs
+
+        const result = listHelper.totalLikes(blogs)
+        assert.strictEqual(36, result)
+    })
 })
--- part4/backend/utils/list_helper.js
+++ part4/backend/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = () => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+
+    const total = blogs.reduce((total, blog) => {
+            return blog.likes + total
+        }, 0)
+    return total
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
