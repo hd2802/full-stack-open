a8a7078d8ca6181b9c2d26709ec640ce730f9da4
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-06-09 20:20:50 +0100

    4.5: adds favouriteBlog and relevant testing

--- part4/backend/tests/list_helper.test.js
+++ part4/backend/tests/list_helper.test.js
@@ -22,18 +22,25 @@ describe('total likes', () => {
         const result = listHelper.totalLikes(blogs)
         assert.strictEqual(result, 0)
     })
-
-    test('when list has only one blog equals the likes of that', () => {
-        const blogs = blogs_data.singleton_blog
-
-        const result = listHelper.totalLikes(blogs)
-        assert.strictEqual(blogs[0].likes, result)
-    })
-
-    test('of a bigger list is calculated right', () => {
-        const blogs = blogs_data.blogs
-
-        const result = listHelper.totalLikes(blogs)
-        assert.strictEqual(36, result)
-    })
 })
+
+describe('favourite blog', () => {
+        test('of empty list is undefined', () => {
+            const blogs = []
+            const result = listHelper.favouriteBlog(blogs)
+            assert.strictEqual(result, null)
+        })
+
+        test('when list has only one blog, returns that blog', () => {
+            const blogs = blogs_data.singleton_blog
+            const result = listHelper.favouriteBlog(blogs)
+            assert.deepStrictEqual(result, blogs[0])
+        })
+
+        test('of a bigger list is the one with most likes', () => {
+            const blogs = blogs_data.blogs
+            const expected = blogs.reduce((fav, blog) => fav.likes > blog.likes ? fav : blog)
+            const result = listHelper.favouriteBlog(blogs)
+            assert.deepStrictEqual(result, expected)
+        })
+    })
--- part4/backend/utils/list_helper.js
+++ part4/backend/utils/list_helper.js
@@ -3,14 +3,27 @@ const dummy = () => {
 }
 
 const totalLikes = (blogs) => {
-
     const total = blogs.reduce((total, blog) => {
             return blog.likes + total
         }, 0)
     return total
 }
 
+const favouriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+    const favourite = blogs.reduce(
+        (favourite, blog) => {
+            return favourite.likes > blog.likes ? favourite : blog
+        }
+    )
+    // console.log(favourite)
+    return favourite
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog
 }
