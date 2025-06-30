d1ed8a216f26ad165ebbaf7d0a0b6ec28385ed86
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-06-30 16:28:24 +0100

    4.19: adding a new blog post only possible if user logged in

--- part4/backend/controllers/blogs.js
+++ part4/backend/controllers/blogs.js
@@ -1,8 +1,18 @@
+const jwt = require('jsonwebtoken')
+
 const blogsRouter = require('express').Router()
-const blog = require('../models/blog')
 const Blog = require('../models/blog')
 
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    
+    if(authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    } 
+    return null
+}
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
@@ -14,7 +24,12 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
 
-    const user = await User.findById(body.userId)
+    const decodedToken = jwt.verify(getTokenFrom(request),process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }  
+    
+    const user = await User.findById(decodedToken.id)
 
     if (!user) {
         return response.status(400).json({ error: 'userID missing or not valid '})
