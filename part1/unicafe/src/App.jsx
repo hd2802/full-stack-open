69ca0ff2e121144c99373228dc5693c48c3c9a03
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-08-21 10:04:11 +0100

    1.8: adds separete components in unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [ good, setGood ] = useState(0)
-  const [ neutral, setNeutral ] = useState(0)
-  const [ bad, setBad ] = useState(0)
+const Statistics = (props) => {
+  const { good, neutral, bad } = props
 
   const calculateAverage = () => {
     if ((good + neutral + bad ) === 0) return 0
@@ -15,14 +13,9 @@ const App = () => {
     return (good / (good+bad+neutral)) * 100
   }
 
+
   return (
     <div>
-      <h1>give feedback</h1>
-      <div>
-        <button onClick={() => setGood(good+1)}>good</button>
-        <button onClick={() => setNeutral(neutral+1)}>neutral</button>
-        <button onClick={() => setBad(bad+1)}>bad</button>
-      </div>
       <h1>statistics</h1>
       <p>
         good {good} <br></br>
@@ -31,6 +24,30 @@ const App = () => {
         average {calculateAverage()} <br></br>
         positive {calculatePositivePercentage()} %
       </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const [ good, setGood ] = useState(0)
+  const [ neutral, setNeutral ] = useState(0)
+  const [ bad, setBad ] = useState(0)
+
+  const props = {
+    good: good,
+    neutral: neutral,
+    bad: bad,
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <div>
+        <button onClick={() => setGood(good+1)}>good</button>
+        <button onClick={() => setNeutral(neutral+1)}>neutral</button>
+        <button onClick={() => setBad(bad+1)}>bad</button>
+      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
