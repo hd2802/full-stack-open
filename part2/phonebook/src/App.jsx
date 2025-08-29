2bca10d11c707fe15324df49c0da44b30504e973
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-08-29 14:05:21 +0100

    2.8: adds number field to phonebook form

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,22 +2,24 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault()
-
-    const nameObject = null
 
     persons.forEach(obj => {
       if (obj.name === newName) {
         alert(`${newName} is already in the phonebook`)
       }
       else {
-        setPersons(persons.concat({name : newName}))
+        setPersons(persons.concat({name : newName,
+          number: newNumber
+        }))
         setNewName('')
+        setNewNumber('')
       }
       }
     )
@@ -25,6 +27,10 @@ const App = () => {
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -35,13 +41,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
       {persons.map(person => 
-        <li key={person.name}>{person.name}</li>
+        <li key={person.name}>{person.name} {person.number}</li>
       )}
       </ul>
     </div>
