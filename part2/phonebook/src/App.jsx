266d86c3507deb2c44607b37f80f7c3874b20b0f
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-08-30 08:35:43 +0100

    2.12: adds post operation to db.json backend in phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -61,11 +61,18 @@ const App = () => {
         alert(`${newName} is already in the phonebook`)
       }
       else {
-        setPersons(persons.concat({name : newName,
+        const newPerson = {
+          name: newName,
           number: newNumber
-        }))
-        setNewName('')
-        setNewNumber('')
+        }
+
+        axios
+          .post('http://localhost:3001/persons', newPerson)
+          .then(response => {
+            setPersons(persons.concat(response.data))
+            setNewName('')
+            setNewNumber('')
+          })
       }
       }
     )
