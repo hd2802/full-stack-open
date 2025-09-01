196ad05a82fad15b92ea722b4f9b3c2745e102d6
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-09-01 16:02:28 +0100

    3.9: links part2 phonebook frontend with new phonebook backend

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+
 const app = express()
 app.use(express.json())
+
+const cors = require('cors')
+app.use(cors())
 
 const morgan = require('morgan')
 app.use(morgan('tiny'))
