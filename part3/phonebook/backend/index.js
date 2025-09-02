ac88b1f04d43038a0c59640b3e3b1335a4ca0f7e
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-09-02 14:09:58 +0100

    3.13: data for phonebook now auto fetched from mongodb

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 app.use(express.json())
@@ -9,106 +11,16 @@ app.use(cors())
 
 const morgan = require('morgan')
 app.use(morgan('tiny'))
-// note: not completing exercise 3.8 - too complicated and not compliant with privacy law so not likely to use in future
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-// helper function for id generation
-const generateId = () => {
-    const maxId = persons.length > 0 ?
-        Math.max(...persons.map(n => Number(n.id)))
-        : 0
-
-    return String(maxId + 1)
-}
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello world </h1>')
-})
-
-app.get('/info', (request, response) => {
-    response.send(`<div>
-            Phonebook has info for ${persons.length} people
-            <br>
-            ${Date()}
-        </div>`)
-})
+const Person = require('./models/person')
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-    
-    if(person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
-})
-
-app.post('/api/persons', (request, response) => {
-    
-    const body = request.body
-
-    if(!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'name or number missing'
-        })
-    }
-
-    // person in persons iterates through the keys/ids 
-    // person of persons iterates through the content itself 
-    for (const person of persons) {
-        if(person.name === body.name) {
-            console.log('found the person')
-            return response.status(400).json({
-                error: 'name must be unique'
-            })
-        }
-    }
-
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: generateId()
-    }
-
-    persons = persons.concat(person)
-    response.json(person)
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
-})
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
