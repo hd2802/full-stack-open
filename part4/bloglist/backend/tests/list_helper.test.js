4fb3314a13031410c981e666799b6f5794596d54
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-09-04 08:34:04 +0100

    4.6&4.7: come back to later

--- part4/bloglist/backend/tests/list_helper.test.js
+++ part4/bloglist/backend/tests/list_helper.test.js
@@ -2,6 +2,8 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
+// note: leaving out parts 4.6 and 4.7 for now
+
 const blogs = [
   {
     _id: "5a422a851b54a676234d17f7",
