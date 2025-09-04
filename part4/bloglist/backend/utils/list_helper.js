681318e6234a3bd8d2b63b68746aebc8d33cbb7b
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-09-04 08:26:16 +0100

    4.4: adds totalLikes and relevant tests (all passing)

--- part4/bloglist/backend/utils/list_helper.js
+++ part4/bloglist/backend/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    var count = 0
+    blogs.forEach(blog => {
+        count += blog.likes
+    })
+    return count
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
