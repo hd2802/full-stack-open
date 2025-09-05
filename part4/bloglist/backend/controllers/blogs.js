94e451510cc33a18aeac7b99b403091d249c5dd8
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-09-05 15:00:47 +0100

    4.22: adds userExtractor to middleware

--- part4/bloglist/backend/controllers/blogs.js
+++ part4/bloglist/backend/controllers/blogs.js
@@ -22,13 +22,7 @@ blogsRouter.get('/:id', async (request, response, next) => {
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
 
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-
-    if(!decodedToken.id) {
-        return response.status(401).json({ error: 'token invalid' })
-    }
-
-    const user = await User.findById(decodedToken.id)
+    const user = request.user
 
     if(!user) {
         return response.status(400).json({ error: 'userId missing or not valid' })
@@ -57,13 +51,7 @@ blogsRouter.post('/', async (request, response, next) => {
 blogsRouter.delete('/:id', async (request, response, next) => {
     const body = request.body
 
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-
-    if(!decodedToken.id) {
-        return response.status(401).json({ error: 'token invalid' })
-    }
-
-    const user = await User.findById(decodedToken.id)
+    const user = request.user
 
     if(!user) {
         return response.status(400).json({ error: 'userId missing or not valid' })
