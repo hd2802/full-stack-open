7449722fd4f738a7d58ea06b11b281cdbc76a799
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-09-05 14:22:07 +0100

    4.19: blogs can only be added by authorized users

--- part4/bloglist/backend/controllers/blogs.js
+++ part4/bloglist/backend/controllers/blogs.js
@@ -1,6 +1,18 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = (request) => {
+    // we pass the authorization field in the payload to request of a blog post creation
+    const authorization = request.get('authorization')
+
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '') // extract just the token from the authorization
+    }
+
+    return null
+}
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
@@ -21,8 +33,13 @@ blogsRouter.get('/:id', async (request, response, next) => {
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
 
-    // currently picks the first user to assign as the blog creator
-    const user = await User.findOne()
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+
+    if(!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
 
     if(!user) {
         return response.status(400).json({ error: 'userId missing or not valid' })
