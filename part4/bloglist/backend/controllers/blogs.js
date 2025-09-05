bc90c843e63791afff46b4b7bee41e31b251cec4
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-09-05 14:38:51 +0100

    4.20: extracts getTokenFrom to middleware tokenExtractor

--- part4/bloglist/backend/controllers/blogs.js
+++ part4/bloglist/backend/controllers/blogs.js
@@ -3,17 +3,6 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = (request) => {
-    // we pass the authorization field in the payload to request of a blog post creation
-    const authorization = request.get('authorization')
-
-    if (authorization && authorization.startsWith('Bearer ')) {
-        return authorization.replace('Bearer ', '') // extract just the token from the authorization
-    }
-
-    return null
-}
-
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
         .find({}).populate('user', { username: 1, name: 1 })
@@ -33,7 +22,7 @@ blogsRouter.get('/:id', async (request, response, next) => {
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
 
-    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
     if(!decodedToken.id) {
         return response.status(401).json({ error: 'token invalid' })
