a102ed7af6f74a11903e630f8bff2d6f2d5d71a7
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-09-06 10:14:05 +0100

    5.6: adds view/hide button for blog details

--- part5/bloglist/frontend/src/components/Blog.jsx
+++ part5/bloglist/frontend/src/components/Blog.jsx
@@ -1,7 +1,29 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [viewAllDetails, setViewAllDetails] = useState(false)
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}<button onClick={() => setViewAllDetails(true)}>view all details</button>
+
+      {viewAllDetails && (
+        <div>
+        {blog.url}
+        <br></br>
+        likes: {blog.likes} <button>like</button>
+        </div>
+      )}
+    </div>  
+  )
+}
 
 export default Blog
