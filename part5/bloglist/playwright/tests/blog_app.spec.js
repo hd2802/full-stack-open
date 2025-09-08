13835344d6ee14eb68dcfcdbfa614619ea76e60b
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-09-08 09:11:33 +0100

    Revert "commit"
    This reverts commit cf3aec5436b885c82bebb7afc3a70d22f653405b.

--- part5/bloglist/playwright/tests/blog_app.spec.js
+++ part5/bloglist/playwright/tests/blog_app.spec.js
@@ -1,7 +1,7 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page, request }) => {
+  beforeEach(async ({ page }) => {
     await page.goto('http://localhost:5173')
   })
 
@@ -9,15 +9,5 @@ describe('Blog app', () => {
     await expect(page.getByText('Log in to application')).toBeVisible()
     await expect(page.getByLabel('username')).toBeVisible()
     await expect(page.getByLabel('password')).toBeVisible()
-  })
-
-  describe('Login', () => {
-    test('succeeds with correct credentials', async ({ page }) => {
-
-    })
-
-    test('fails with wrong credentials', async ({ page }) => {
-        
-    })
   })
 })
