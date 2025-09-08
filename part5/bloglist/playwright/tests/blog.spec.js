4a3f1f55e97e58a971f217227aca23b5bf51a502
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-09-08 14:46:24 +0100

    5.17: adds playwright test for loginform being present

---
+++ part5/bloglist/playwright/tests/blog.spec.js
@@ -0,0 +1,14 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.locator('text=Log in to application')).toBeVisible()
+    await expect(page.locator('input[type="text"]')).toBeVisible()
+    await expect(page.locator('input[type="password"]')).toBeVisible()
+    await expect(page.locator('button', { hasText: 'login' })).toBeVisible()
+  })
+})
