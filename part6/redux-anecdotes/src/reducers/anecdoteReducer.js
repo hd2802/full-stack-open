9fd5341f5a0566263db749813d89e7a6932fc97c
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-09-10 10:11:15 +0100

    6.16: initialises using asynchronous action creators

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,14 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
+import anecdoteService from '../services/anecdotes'
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
@@ -41,57 +32,11 @@ export const {
   setAnecdotes
  } = anecdoteSlice.actions
 
+ export const initialiseAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+ }
+
 export default anecdoteSlice.reducer
-
-
-/** 
-export const reducerVote = (id) => {
-  return {
-    type: 'VOTE',
-    payload: {
-      id
-    }
-  }
-}
-
-export const createAnecdote = (content) => {
-    return {
-        type: 'NEW_ANECDOTE',
-        payload: {
-            content,
-            id: getId(),
-        }
-    }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
-
-const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-
-  switch (action.type) {
-    case 'VOTE':
-      const id = action.payload.id
-      // need to find the matching anecdote to the id 
-      // then update the number of votes for that specific anecdote
-      // then need to keep the rest of the ancedotes the same
-      // state is an array of objects so we can use the map function on it 
-      const newState = state.map(anecdote => (
-        anecdote.id === id 
-          // need to copy all the current values FIRST, before overriding the desired value
-          ? { ... anecdote, votes: anecdote.votes + 1}
-          : anecdote
-      ))
-      return newState
-    
-    case 'NEW_ANECDOTE':
-      return [... state, {id: action.payload.id, content: action.payload.content, votes: 0}]
-
-    default:
-      return state
-  }
-}
-
-export default reducer
-*/
