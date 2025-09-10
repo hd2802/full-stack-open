806c01de856d61cd0aea4eb0d4d577327c71e8b8
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-09-10 02:43:53 +0100

    6.11: uses redux toolkit and slice for anecdote reducer

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import { createSlice } from '@reduxjs/toolkit'
+
 const anecdotesAtStart = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
@@ -17,6 +19,37 @@ const asObject = (anecdote) => {
   }
 }
 
+const initialState = anecdotesAtStart.map(asObject)
+
+const anecdoteSlice = createSlice({
+  name: 'anecdotes',
+  initialState,
+  reducers: {
+    createAnecdote(state, action) {
+      const content = action.payload
+      state.push({
+        content,
+        id: getId(),
+        votes: 0
+      })
+    },
+    reducerVote(state, action) {
+      const id = action.payload
+     return state.map(anecdote => (
+      anecdote.id === id 
+        ? {... anecdote, votes: anecdote.votes + 1}
+        : anecdote
+     ))
+    }
+  }
+})
+
+export const {createAnecdote, reducerVote} = anecdoteSlice.actions
+
+export default anecdoteSlice.reducer
+
+
+/** 
 export const reducerVote = (id) => {
   return {
     type: 'VOTE',
@@ -66,3 +99,4 @@ const reducer = (state = initialState, action) => {
 }
 
 export default reducer
+*/
