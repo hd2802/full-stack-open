1ef7bbc25425df89c93e94bd283183ffd83bbafe
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-09-10 02:36:28 +0100

    6.10: uses redux toolkit for filter reducer

--- part6/redux-anecdotes/src/components/Filter.jsx
+++ part6/redux-anecdotes/src/components/Filter.jsx
@@ -1,11 +1,11 @@
 import { useDispatch } from 'react-redux'
-import { filterChange } from '../reducers/filterReducer'
+import { setFilter } from '../reducers/filterReducer'
 
 const Filter = () => {
     const dispatch = useDispatch()
     
     const search = (event) => {
-        dispatch(filterChange(event.target.value))
+        dispatch(setFilter(event.target.value))
     }
 
     return (
--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,3 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const filterSlice = createSlice({
+    name: 'filter',
+    initialState: '',
+    reducers: {
+        setFilter: (state, action) => {
+            return action.payload
+        }
+    }
+})
+
+export const { setFilter } = filterSlice.actions
+
+export default filterSlice.reducer
+
+/**
 const filterReducer = (state = 'ALL', action) => {
     switch (action.type) {
         case 'SET_FILTER':
@@ -15,3 +32,4 @@ export const filterChange = filter => {
 }
 
 export default filterReducer
+*/
